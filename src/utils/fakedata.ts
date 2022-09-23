e1242fb4be52d787cb0ff774fbbee67d8ea1c5a0
Joseph Infante <josephinfante@Josephs-MacBook-Pro.local>
2022-09-22 21:41:40 -0500

    Adding fake data file to build the task list view

---
+++ src/utils/fakedata.ts
@@ -0,0 +1,51 @@
+export const fakedata = [
+    {
+        "id": "id001",
+        "name": "My first task",
+        "description": "My description",
+        "created_at": new Date(),
+        "updated_at": new Date()
+    },
+    {
+        "id": "id002",
+        "name": "My second task",
+        "description": "My description",
+        "created_at": new Date(),
+        "updated_at": new Date()
+    },
+    {
+        "id": "id003",
+        "name": "My third task",
+        "description": "My description",
+        "created_at": new Date(),
+        "updated_at": new Date()
+    },
+    {
+        "id": "id004",
+        "name": "My fourth task",
+        "description": "My description",
+        "created_at": new Date(),
+        "updated_at": new Date()
+    },
+    {
+        "id": "id005",
+        "name": "My fifth task",
+        "description": "My description",
+        "created_at": new Date(),
+        "updated_at": new Date()
+    },
+    {
+        "id": "id006",
+        "name": "My sixth task",
+        "description": "My description",
+        "created_at": new Date(),
+        "updated_at": new Date()
+    },
+    {
+        "id": "id007",
+        "name": "My seventh task",
+        "description": "My description",
+        "created_at": new Date(),
+        "updated_at": new Date()
+    }
+]
