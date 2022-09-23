76670b6da8c8ddee6277d0181caae413739ec0a6
Joseph Infante <josephinfante@Josephs-MacBook-Pro.local>
2022-09-22 21:53:02 -0500

    Adding a condition for when the user is trying to navigate to their profile

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,25 +1,39 @@
-import { Text } from "native-base";
 import { useState } from "react";
 import { CustomButton, CustomContainer } from "../../components";
 import { StyleSheet, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { colors } from "../../styles";
+import { CustomList } from "./components/CustomList";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { getItem } from "../../utils";
 
 export interface HomeInterface {}
 
 const Home: React.FC<HomeInterface> = () => {
   const navigation: any = useNavigation();
-  const [press, setPressed] = useState(false);
+  const userProfile = async () => {
+    let token = await getItem("token");
+    !token ? navigation.navigate("SignIn") : navigation.navigate("Profile");
+  }
   return (
     <CustomContainer>
-      <Text style={{ marginBottom: 50, fontSize: 20 }}>Todo List</Text>
+      <CustomList />
       <CustomButton
         placeholder="Add a task"
         onPress={() => navigation.navigate("Task")}
         containerStyles={styles.containerStyles}
-        placeholderStyles={styles.placeholderStyles}
-        buttonStyles={styles.buttonStyles}
       />
+      <TouchableOpacity
+        style={styles.userProfile}
+        onPress={userProfile}
+      >
+        <FontAwesome5
+          name={"user"}
+          size={20}
+          color={colors.light}
+          style={{ alignSelf: "center" }}
+        />
+      </TouchableOpacity>
     </CustomContainer>
   );
 };
@@ -29,18 +43,18 @@ export default Home;
 const styles = StyleSheet.create({
   containerStyles: {
     position: "absolute",
-    bottom: 50,
+    bottom: 100,
     right: 20,
   },
-  buttonStyles: {
+  userProfile: {
+    position: "absolute",
+    bottom: 50,
+    right: 20,
     backgroundColor: colors.purpleLight,
-    borderRadius: 10,
-  },
-  placeholderStyles: {
-    fontSize: 15,
-    alignSelf: "center",
+    width: 40,
+    height: 40,
     justifyContent: "center",
-    paddingHorizontal: 10,
-    paddingVertical: 5,
+    alignContent: "center",
+    borderRadius: 50,
   },
 });
