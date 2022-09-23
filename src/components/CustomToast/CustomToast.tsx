f57f45121bccb9ed2251050ae04e4cf51265b996
Joseph Infante <josephinfante@Josephs-MacBook-Pro.local>
2022-09-23 01:50:26 -0500

    Adding animated view and timings to the toast component

--- src/components/CustomToast/CustomToast.tsx
+++ src/components/CustomToast/CustomToast.tsx
@@ -1,67 +1,144 @@
 import { MotiView } from "moti";
 import { Text, View } from "native-base";
-import React, { useEffect, useState } from "react";
-import { SafeAreaView, StyleSheet } from "react-native";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import {
+  DeviceEventEmitter,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  ToastAndroid,
+} from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 import { colors } from "../../styles";
+import { SHOW_TOAST_MESSAGE } from "./event/toast.event";
 
-export interface CustomToastInterface {
-  text?: string;
+export interface CustomToastInterface {}
+
+interface Data {
+  message: string;
+  type: "success" | "error" | "warning";
+  duration: number;
+  useNativeToast: boolean;
 }
 
-const CustomToast: React.FC<CustomToastInterface> = (
-  props: CustomToastInterface
-) => {
+const CustomToast: React.FC<CustomToastInterface> = () => {
+  const [messageType, setMessageType] = useState<
+    "success" | "warning" | "error"
+  >("success");
+  const timeOutRef = useRef<any>();
+  const animatedOpacity = useSharedValue(0);
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: animatedOpacity.value,
+    };
+  }, []);
+  const [timeOutDuration, setTimeOutDuration] = useState(5000);
+  const [message, setMessage] = useState("");
+  const onNewToast = (data: Data) => {
+    if (Platform.OS === "android" && data.useNativeToast) {
+      return ToastAndroid.show(data.message, ToastAndroid.LONG);
+    }
+    if (data.duration) {
+      setTimeOutDuration(data.duration);
+    }
+    setMessage(data.message);
+    setMessageType(data.type);
+  };
+  const closeToast = useCallback(() => {
+    setMessage("");
+    setTimeOutDuration(timeOutDuration);
+    animatedOpacity.value = withTiming(0);
+    clearInterval(timeOutRef.current);
+  }, [animatedOpacity]);
+  useEffect(() => {
+    if (message) {
+      timeOutRef.current = setInterval(() => {
+        if (timeOutDuration === 0) {
+          closeToast();
+        } else {
+          setTimeOutDuration(timeOutDuration - 1000);
+        }
+      }, 500);
+    }
+    return () => {
+      clearInterval(timeOutRef.current);
+    };
+  }, [closeToast, message, timeOutDuration]);
+  useEffect(() => {
+    if (message) {
+      animatedOpacity.value = withTiming(1, { duration: 1000 });
+    }
+  }, [message, animatedOpacity]);
+
+  useEffect(() => {
+    DeviceEventEmitter.addListener(SHOW_TOAST_MESSAGE, onNewToast);
+    return () => {
+      DeviceEventEmitter.removeAllListeners();
+    };
+  }, []);
+  if (!message) {
+    return null;
+  }
   return (
-    <View style={styles.toastContainer}>
-      <MotiView
-        from={{ opacity: 0, translateY: -50 }}
-        animate={{ opacity: 1, translateY: 0 }}
-        delay={100}
-        transition={{
-          type: "spring",
-        }}
-        exit={{
-          translateY: -50,
-        }}
-      >
-        <View
-          _light={{ bg: colors.secondaryLight }}
-          _dark={{ bg: colors.secondaryDark }}
-          style={{
-            borderRadius: 20,
+    <Animated.View style={[styles.toastContainer, animatedStyle]}>
+      <View>
+        <MotiView
+          from={{ opacity: 0, translateY: -50 }}
+          animate={{ opacity: 1, translateY: 0 }}
+          delay={100}
+          transition={{
+            type: "spring",
+          }}
+          exit={{
+            translateY: -50,
           }}
         >
-          <SafeAreaView></SafeAreaView>
-          <View style={styles.container}>
-            <MotiView
-              from={{ opacity: 0, translateY: -50 }}
-              animate={{ opacity: 1, translateY: 0 }}
-              delay={150}
-              transition={{
-                type: "spring",
-              }}
-              exit={{
-                translateY: -50,
-              }}
-              style={styles.statusBar}
-            ></MotiView>
-            <MotiView
-              from={{ opacity: 0, translateY: -50 }}
-              animate={{ opacity: 1, translateY: 0 }}
-              delay={170}
-              transition={{
-                type: "spring",
-              }}
-              exit={{
-                translateY: -50,
-              }}
-            >
-              <Text>The task was added correctly</Text>
-            </MotiView>
+          <View
+            _light={{ bg: colors.secondaryLight }}
+            _dark={{ bg: colors.secondaryDark }}
+            style={{
+              borderRadius: 20,
+            }}
+          >
+            <SafeAreaView></SafeAreaView>
+            <View style={styles.container}>
+              <MotiView
+                from={{ opacity: 0, translateY: -50 }}
+                animate={{ opacity: 1, translateY: 0 }}
+                delay={150}
+                transition={{
+                  type: "spring",
+                }}
+                exit={{
+                  translateY: -50,
+                }}
+                style={[
+                  styles.statusBar,
+                  { backgroundColor: colors[messageType] },
+                ]}
+              ></MotiView>
+              <MotiView
+                from={{ opacity: 0, translateY: -50 }}
+                animate={{ opacity: 1, translateY: 0 }}
+                delay={170}
+                transition={{
+                  type: "spring",
+                }}
+                exit={{
+                  translateY: -50,
+                }}
+              >
+                <Text>{message}</Text>
+              </MotiView>
+            </View>
           </View>
-        </View>
-      </MotiView>
-    </View>
+        </MotiView>
+      </View>
+    </Animated.View>
   );
 };
 
@@ -87,7 +164,6 @@ const styles = StyleSheet.create({
     width: 4,
     height: 40,
     borderRadius: 50,
-    backgroundColor: colors.green,
     marginRight: 5,
   },
 });
