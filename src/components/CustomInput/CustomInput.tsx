bbeaabdab086acf3b69fc3c8d8615c454888be25
Joseph Infante <josephinfante@Josephs-MacBook-Pro.local>
2022-09-22 21:47:12 -0500

    Creating a custom input component

---
+++ src/components/CustomInput/CustomInput.tsx
@@ -0,0 +1,46 @@
+import { MotiView } from "moti";
+import { Input } from "native-base";
+import { StyleSheet } from "react-native";
+
+export interface CustomInputInterface {
+  placeholder: string;
+  delay: number;
+  onChangeText?: (text: string) => void;
+  containerStyles?: {};
+  inputStyles?: {};
+}
+
+const CustomInput: React.FC<CustomInputInterface> = (
+  props: CustomInputInterface
+) => {
+  return (
+    <MotiView
+      style={[styles.container, props.containerStyles]}
+      from={{ opacity: 0, translateX: -15 }}
+      animate={{ opacity: 1, translateX: 0 }}
+      delay={props.delay}
+      transition={{
+        type: "spring",
+      }}
+      exit={{
+        opacity: 0,
+      }}
+    >
+      <Input
+        style={props.inputStyles}
+        variant="unstyled"
+        placeholder={props.placeholder}
+        w="100%"
+        onChangeText={props.onChangeText}
+      />
+    </MotiView>
+  );
+};
+
+export default CustomInput;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 20,
+  },
+});
