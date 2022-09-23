bd91471753c770da3b5342e3a4386c8ffd0ef455
Joseph Infante <josephinfante@Josephs-MacBook-Pro.local>
2022-09-22 21:43:20 -0500

    Creating a style file for the color schema of the app

---
+++ src/styles/colors.styles.tsx
@@ -0,0 +1,12 @@
+export const colors = {
+    dark: '#000807',
+    secondaryDark: '#121313',
+    light: '#FBF9FF',
+    secondaryLight: '#FFFFFF',
+    purpleLight: '#B3B7EE',
+    purpleDark: '#A2A3BB',
+    yellow: '#FCFC62',
+    red: '#B3001B',
+    green: '#08AC4A',
+    link: '#03A5DB'
+}
