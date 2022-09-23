e43c96fb004725dded7b4c9788cc43524142b33e
Joseph Infante <josephinfante@Josephs-MacBook-Pro.local>
2022-09-22 21:35:16 -0500

    Importing navigation component, and adding redux provider

--- App.tsx
+++ App.tsx
@@ -1,26 +1,19 @@
+import "react-native-gesture-handler";
 import "react-native-reanimated";
 import React from "react";
 import { NativeBaseProvider } from "native-base";
-import { CustomSwitch, CustomToast } from "./src/components";
-import { Home, Task } from "./src/pages";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { CustomSwitch } from "./src/components";
+import { MainNavigation } from "./src/navigation/MainNavigation";
+import { Provider } from "react-redux";
+import { store } from "./src/redux/store";
 
 export default function App() {
-  const Stack = createNativeStackNavigator();
   return (
     <NativeBaseProvider>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="Task" component={Task} />
-        </Stack.Navigator>
-      </NavigationContainer>
-      <CustomSwitch />
+      <Provider store={store}>
+        <MainNavigation />
+        <CustomSwitch />
+      </Provider>
     </NativeBaseProvider>
   );
 }
