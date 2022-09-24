a92a579a50ff94389c4ebe602639fc349cbf0db0
Joseph Infante <josephinfante@Josephs-MacBook-Pro.local>
2022-09-24 14:56:56 -0500

    Adding loader after user stars the signin process

--- src/pages/SignIn-SignUp/SignIn/SignIn.tsx
+++ src/pages/SignIn-SignUp/SignIn/SignIn.tsx
@@ -2,18 +2,21 @@ import { useNavigation } from "@react-navigation/native";
 import { Text, View } from "native-base";
 import React, { useEffect, useState } from "react";
 import { Keyboard, StyleSheet, TouchableOpacity } from "react-native";
+import { useDispatch } from "react-redux";
 import {
   CustomButton,
   CustomContainer,
   CustomInput,
 } from "../../../components";
 import { toast } from "../../../components/CustomToast/event/toast.event";
+import { resetLoading, setLoading } from "../../../redux/states/loader.state";
 import { colors, containerStyles } from "../../../styles";
 import { SignInHook } from "./hooks/SignIn.hooks";
 
 export interface SignInInterface {}
 
 const SignIn: React.FC<SignInInterface> = () => {
+  const dispatch = useDispatch();
   const [signIn, setSignIn] = useState<{ email: string; password: string }>({
     email: "",
     password: "",
@@ -27,74 +30,78 @@ const SignIn: React.FC<SignInInterface> = () => {
   }, []);
 
   const handleSignIn = async () => {
+    dispatch(setLoading({loading: true}));
     let response = await SignInHook(signIn);
+    if (!response) { dispatch(resetLoading()); return; }
     if (response) {
       toast.success({message: `Welcome back, ${response.name}!`, duration: 2000})
       setTimeout(() => {
         navigation.goBack();
+        dispatch(resetLoading());
       }, 2500);
     }
+    
   };
   return (
-    <TouchableOpacity
-      onPress={Keyboard.dismiss}
-      style={{ width: "100%", height: "100%" }}
-      activeOpacity={1}
-    >
-      <CustomContainer>
-        <View style={containerStyles.container}>
-          {view ? (
-            <>
-              <CustomInput
-                containerStyles={styles.containerStyles}
-                inputStyles={styles.placeholderStyles}
-                placeholder="Email"
-                delay={220}
-                onChangeText={(text: string) => {
-                  setSignIn({
-                    ...signIn,
-                    email: text,
-                  });
-                }}
-              />
-              <CustomInput
-                containerStyles={styles.containerStyles}
-                inputStyles={styles.placeholderStyles}
-                placeholder="Password"
-                delay={270}
-                onChangeText={(text: string) => {
-                  setSignIn({
-                    ...signIn,
-                    password: text,
-                  });
-                }}
-              />
-            </>
-          ) : null}
-          <CustomButton
-            placeholder="Sign In"
-            onPress={() => handleSignIn()}
-            buttonStyles={styles.buttonContainer}
-          />
+      <TouchableOpacity
+        onPress={Keyboard.dismiss}
+        style={{ width: "100%", height: "100%" }}
+        activeOpacity={1}
+      >
+        <CustomContainer>
+          <View style={containerStyles.container}>
+            {view ? (
+              <>
+                <CustomInput
+                  containerStyles={styles.containerStyles}
+                  inputStyles={styles.placeholderStyles}
+                  placeholder="Email"
+                  delay={220}
+                  onChangeText={(text: string) => {
+                    setSignIn({
+                      ...signIn,
+                      email: text,
+                    });
+                  }}
+                />
+                <CustomInput
+                  containerStyles={styles.containerStyles}
+                  inputStyles={styles.placeholderStyles}
+                  placeholder="Password"
+                  delay={270}
+                  onChangeText={(text: string) => {
+                    setSignIn({
+                      ...signIn,
+                      password: text,
+                    });
+                  }}
+                />
+              </>
+            ) : null}
+            <CustomButton
+              placeholder="Sign In"
+              onPress={() => handleSignIn()}
+              buttonStyles={styles.buttonContainer}
+            />
 
-          <View style={styles.signUpContainer}>
-            <Text>You don't have an account yet?</Text>
-            <TouchableOpacity
-              onPress={() => {
-                navigation.navigate("SignUp");
-              }}
-            >
-              <Text style={styles.link}>Sign Up</Text>
-            </TouchableOpacity>
+            <View style={styles.signUpContainer}>
+              <Text>You don't have an account yet?</Text>
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate("SignUp");
+                }}
+              >
+                <Text style={styles.link}>Sign Up</Text>
+              </TouchableOpacity>
+            </View>
           </View>
-        </View>
-        <CustomButton
-          placeholder="Back"
-          onPress={() => navigation.goBack()}
-          buttonStyles={{ position: "absolute", bottom: 50, right: 20 }}
-        />
-      </CustomContainer>
-    </TouchableOpacity>
+          <CustomButton
+            placeholder="Back"
+            onPress={() => navigation.goBack()}
+            buttonStyles={{ position: "absolute", bottom: 50, right: 20 }}
+          />
+        </CustomContainer>
+      </TouchableOpacity>
   );
 };
 
