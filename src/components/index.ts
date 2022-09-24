dc23203468b02b8e24ce7e9e6f2633e34dd20842
Joseph Infante <josephinfante@Josephs-MacBook-Pro.local>
2022-09-24 14:59:33 -0500

    Updating component barel

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export * from './CustomButton';
 export * from './CustomContainer';
 export * from './CustomInput';
+export * from './CustomLoader';
 export * from './CustomSwitch';
 export * from './CustomToast';
