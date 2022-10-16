6d62c12a82a59e24573ae676efe8cf7852ff224f
Joseph Infante <josephinfante@Josephs-MacBook-Pro.local>
2022-10-16 17:23:05 -0500

    Adding platform condition to the list

--- src/pages/Home/components/List/List.tsx
+++ src/pages/Home/components/List/List.tsx
@@ -1,24 +1,39 @@
+import { useNavigation } from "@react-navigation/native";
 import { View } from "native-base";
 import React from "react";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, TouchableOpacity } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { CustomContainer } from "../../../../components";
 import { TaskCreated } from "../../../../interfaces";
 import { Item } from "../Item";
+import { Platform } from 'react-native';
 
 export interface ListInterface {
   tasks: TaskCreated[];
 }
 
 const List: React.FC<ListInterface> = ({ tasks }) => {
+  const navigation: any = useNavigation();
   return (
     <CustomContainer>
-      <View style={{flex: 1}}>
-		<SafeAreaView></SafeAreaView>
-        <ScrollView>
-          {tasks.map((task: TaskCreated) => {
-			return <Item key={task._id} task={task}/>;
-          })}
+      <View style={{flex: 1, marginBottom: 100}}>
+		    <SafeAreaView></SafeAreaView>
+        <ScrollView contentContainerStyle={{paddingVertical: 20}}>
+          {
+            tasks.map((task: TaskCreated) => {
+              return (
+                Platform.OS === 'ios'
+                ? <Item key={task._id} task={task}/>
+                :(
+                  <TouchableOpacity 
+                    onPress={() => navigation.navigate("Task", task)}
+                    key={task._id}>
+                    <Item task={task}/>
+                  </TouchableOpacity>
+                )
+              )
+            })
+          }
         </ScrollView>
       </View>
     </CustomContainer>
